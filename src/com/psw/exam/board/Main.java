ffb06b4884ae69f046cf676a3852980790440b66
chanhyeok1 <kongkch830@gmail.com>
2022-08-23 15:03:14 +0900

    등록 할 때마다 게시물 번호 증가

--- src/com/psw/exam/board/Main.java
+++ src/com/psw/exam/board/Main.java
@@ -5,19 +5,21 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    int c = 0;
 
-    System.out.println("=== 게시판 v 0.1 ===");
-    System.out.println("=== 프로그램 시작 ===");
     while(true){
+      c++;
       System.out.printf("명령)");
       String cmd = sc.nextLine();
-      if(cmd.equals("exit")){
-        break;
+      if(cmd.equals("/user/article/write")){
+        System.out.printf("title)");
+        cmd = sc.nextLine();
+        System.out.printf("내용)");
+        cmd = sc.nextLine();
+        System.out.printf("%d번 게시물이 등록 되었습니다.\n", c);
       }
-      System.out.printf("입력 받은 명령어 : %s\n", cmd);
     }
-    System.out.println("=== 프로그램 종료 ===");
 
-    sc.close();
+    //sc.close();
   }
 }
